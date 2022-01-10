f46abb751abc1e3553b1633ac49f899d06b88990
yasirarafa <yasir.arafat1111@gmail.com>
2022-01-10 17:54:45 +0530

    Commit for test

---
+++ news/src/components/NewsItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                This is the news item
+            </div>
+        )
+    }
+}
+
+export default NewsItem
